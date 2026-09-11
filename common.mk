9a72f2f54d8a74c7c488af40b870fb83863743f6
CuriousNom <aryanbiswas7585@gmail.com>
2026-09-11 15:39:39 +0530

    common: Move common defs and symlinks to hardware/qcom-caf/common
    Change-Id: I7ccd9daaa4a61b7a6e02adc7c59a38da2ac9246d

--- common.mk
+++ common.mk
@@ -136,10 +136,6 @@ else ifeq ($(call is-board-platform-in-list,$(3_18_FAMILY)),true)
 TARGET_KERNEL_VERSION ?= 3.18
 endif
 
-ifneq (,$(filter 3.18 4.4 4.9 4.14 4.19 5.4, $(TARGET_KERNEL_VERSION)))
-$(call soong_config_set,qti_thermal,netlink,false)
-endif
-
 ifeq ($(call is-board-platform-in-list,$(QCOM_BOARD_PLATFORMS)),true)
 ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
 # Compatibility matrix
@@ -211,113 +207,5 @@ PRODUCT_VENDOR_PROPERTIES += \
 # System Enable QCOM enhanced feature
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.vendor.qti.va_aosp.support=1
-
-# RFS APQ GNSS symlinks
-PRODUCT_PACKAGES += \
-    rfs_apq_gnss_hlos_symlink \
-    rfs_apq_gnss_ramdumps_symlink \
-    rfs_apq_gnss_readonly_firmware_symlink \
-    rfs_apq_gnss_readonly_vendor_firmware_symlink \
-    rfs_apq_gnss_readwrite_symlink \
-    rfs_apq_gnss_shared_symlink
-
-# RFS MDM ADSP symlinks
-PRODUCT_PACKAGES += \
-    rfs_mdm_adsp_hlos_symlink \
-    rfs_mdm_adsp_ramdumps_symlink \
-    rfs_mdm_adsp_readonly_firmware_symlink \
-    rfs_mdm_adsp_readonly_vendor_firmware_symlink \
-    rfs_mdm_adsp_readwrite_symlink \
-    rfs_mdm_adsp_shared_symlink
-
-# RFS MDM CDSP symlinks
-PRODUCT_PACKAGES += \
-    rfs_mdm_cdsp_hlos_symlink \
-    rfs_mdm_cdsp_ramdumps_symlink \
-    rfs_mdm_cdsp_readonly_firmware_symlink \
-    rfs_mdm_cdsp_readonly_vendor_firmware_symlink \
-    rfs_mdm_cdsp_readwrite_symlink \
-    rfs_mdm_cdsp_shared_symlink
-
-# RFS MDM MPSS symlinks
-PRODUCT_PACKAGES += \
-    rfs_mdm_mpss_hlos_symlink \
-    rfs_mdm_mpss_ramdumps_symlink \
-    rfs_mdm_mpss_readonly_firmware_symlink \
-    rfs_mdm_mpss_readonly_vendor_firmware_symlink \
-    rfs_mdm_mpss_readwrite_symlink \
-    rfs_mdm_mpss_shared_symlink
-
-# RFS MDM SLPI symlinks
-PRODUCT_PACKAGES += \
-    rfs_mdm_slpi_hlos_symlink \
-    rfs_mdm_slpi_ramdumps_symlink \
-    rfs_mdm_slpi_readonly_firmware_symlink \
-    rfs_mdm_slpi_readonly_vendor_firmware_symlink \
-    rfs_mdm_slpi_readwrite_symlink \
-    rfs_mdm_slpi_shared_symlink
-
-# RFS MDM TN symlinks
-PRODUCT_PACKAGES += \
-    rfs_mdm_tn_hlos_symlink \
-    rfs_mdm_tn_ramdumps_symlink \
-    rfs_mdm_tn_readonly_firmware_symlink \
-    rfs_mdm_tn_readonly_vendor_firmware_symlink \
-    rfs_mdm_tn_readwrite_symlink \
-    rfs_mdm_tn_shared_symlink
-
-# RFS MDM WPSS symlinks
-PRODUCT_PACKAGES += \
-    rfs_mdm_wpss_hlos_symlink \
-    rfs_mdm_wpss_ramdumps_symlink \
-    rfs_mdm_wpss_readonly_firmware_symlink \
-    rfs_mdm_wpss_readonly_vendor_firmware_symlink \
-    rfs_mdm_wpss_readwrite_symlink \
-    rfs_mdm_wpss_shared_symlink
-
-# RFS MSM ADSP symlinks
-PRODUCT_PACKAGES += \
-    rfs_msm_adsp_hlos_symlink \
-    rfs_msm_adsp_ramdumps_symlink \
-    rfs_msm_adsp_readonly_firmware_symlink \
-    rfs_msm_adsp_readonly_vendor_firmware_symlink \
-    rfs_msm_adsp_readwrite_symlink \
-    rfs_msm_adsp_shared_symlink
-
-# RFS MSM CDSP symlinks
-PRODUCT_PACKAGES += \
-    rfs_msm_cdsp_hlos_symlink \
-    rfs_msm_cdsp_ramdumps_symlink \
-    rfs_msm_cdsp_readonly_firmware_symlink \
-    rfs_msm_cdsp_readonly_vendor_firmware_symlink \
-    rfs_msm_cdsp_readwrite_symlink \
-    rfs_msm_cdsp_shared_symlink
-
-# RFS MSM MPSS symlinks
-PRODUCT_PACKAGES += \
-    rfs_msm_mpss_hlos_symlink \
-    rfs_msm_mpss_ramdumps_symlink \
-    rfs_msm_mpss_readonly_firmware_symlink \
-    rfs_msm_mpss_readonly_vendor_firmware_symlink \
-    rfs_msm_mpss_readwrite_symlink \
-    rfs_msm_mpss_shared_symlink
-
-# RFS MSM SLPI symlinks
-PRODUCT_PACKAGES += \
-    rfs_msm_slpi_hlos_symlink \
-    rfs_msm_slpi_ramdumps_symlink \
-    rfs_msm_slpi_readonly_firmware_symlink \
-    rfs_msm_slpi_readonly_vendor_firmware_symlink \
-    rfs_msm_slpi_readwrite_symlink \
-    rfs_msm_slpi_shared_symlink
-
-# RFS MSM WPSS symlinks
-PRODUCT_PACKAGES += \
-    rfs_msm_wpss_hlos_symlink \
-    rfs_msm_wpss_ramdumps_symlink \
-    rfs_msm_wpss_readonly_firmware_symlink \
-    rfs_msm_wpss_readonly_vendor_firmware_symlink \
-    rfs_msm_wpss_readwrite_symlink \
-    rfs_msm_wpss_shared_symlink
 
 endif # QCOM_BOARD_PLATFORMS
